03a2b69717c4709d88c99d1ad40ac0987201a5c6
archHavik <hayden.robbins.mk1@gmail.com>
2025-09-30 14:08:19 -0600

    Added logic to highlight custom keycodes

--- keyboards/keychron/v6_max/ansi_encoder/keymaps/archHavik/keymap.c
+++ keyboards/keychron/v6_max/ansi_encoder/keymaps/archHavik/keymap.c
@@ -25,6 +25,8 @@ enum layers {
 
 enum custom_keycodes {
 	QMKFLASH = SAFE_RANGE,
+    APT_UPDATE,
+    LAST
 };
 
 // clang-format on
@@ -35,6 +37,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	// Macros
 	switch (keycode) {
+		case APT_UPDATE:
+			if (record->event.pressed) {
+				//when key is pressed
+				SEND_STRING("sudo apt update && sudo apt -y upgrade\n");
+			} else {
+				//when key is released
+			}
+			break;
 		case QMKFLASH:
 			if (record->event.pressed) {
 				//when key is pressed
@@ -67,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
     [WIN_BASE] = LAYOUT_ansi_109(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_CTANA, RGB_MOD,  _______,  _______,  _______,  _______,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_CTANA, RGB_MOD,  _______,  _______,  _______,  APT_UPDATE,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
@@ -108,6 +118,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 	uint8_t layer = get_highest_layer(layer_state);
 	rgb_matrix_set_color(layer + 1, LAYER_INDICATOR_COLOR);
 
+    //Set indicator color to keys assigned to things on the function layers
 	if (layer == MAC_FN || layer == WIN_FN) {
 		for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
@@ -119,6 +130,19 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             }
         }
 	}
+    else if ( layer == MAC_BASE || layer == WIN_BASE ) {
+        for (int key = QMKFLASH; key < LAST; ++key) {
+            for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+                for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                    uint8_t index = g_led_config.matrix_co[row][col];
+
+                    if (keymap_key_to_keycode(layer, (keypos_t){col,row}) == key) {
+                        rgb_matrix_set_color(index, INDICATOR_COLOR);
+                    }
+                }
+            }
+        }
+    }
     return false;
 }
 
